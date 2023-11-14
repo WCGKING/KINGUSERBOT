d9c08462ad3304684b7d21549b3d1d1f8bf4bfe7
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-11-14 21:49:11 +0530

    KING

--- King/plugins/alive.py
+++ King/plugins/alive.py
@@ -2,11 +2,11 @@ from .. import *
 
 @app.on_message(commandx(["alive"]))
 async def alive_check(client, message):
-    await message.reply_text("**🥀 I Aᴍ Aʟɪᴠᴇ Mʏ Dᴇᴀʀ Branded Mᴀsᴛᴇʀ ✨ ...**")
+    await message.reply_text("**🥀 𝗜 𝗔𝗠 𝗔𝗟𝗜𝗩𝗘 𝗠𝗬 𝗗𝗘𝗔𝗥 𝗕𝗥𝗔𝗡𝗗𝗘𝗗 𝗞𝗜𝗡𝗚 𝗠𝗔𝗦𝗧𝗘𝗥✨ ...**")
 
 
 
-__NAME__ = "Alive"
+__NAME__ = "✨ Alive 🌷"
 __MENU__ = f"""
 **🥀 Check Userbot Working
 Or Not ..**
