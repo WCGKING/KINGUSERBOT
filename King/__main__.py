b064341e084de3696812e89dc981c10dc9389ae7
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-11-14 20:16:17 +0530

    KING

--- King/__main__.py
+++ King/__main__.py
@@ -3,7 +3,7 @@ import importlib
 
 from pytgcalls import idle
 
-from . import Daxx as client
+from . import King as client
 from .import PLUGINS, log
 from .plugins import ALL_PLUGINS
 
@@ -15,7 +15,7 @@ async def aditya():
     log.info("Importing all plugins ...")
     for all_plugin in ALL_PLUGINS:
         imported_plugin = importlib.import_module(
-            "Daxx.plugins." + all_plugin)
+            "King.plugins." + all_plugin)
         if (hasattr(imported_plugin, "__NAME__"
            ) and imported_plugin.__NAME__):
             imported_plugin.__NAME__ = imported_plugin.__NAME__
