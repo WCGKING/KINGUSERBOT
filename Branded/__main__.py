0513e86f9570fa8c37dff4fe2bc3c100d9493790
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-11-15 14:32:54 +0530

    𝗕𝗥𝗔𝗡𝗗𝗘𝗗 𓆩🇽𓆪 𝗞𝗜𝗡𝗚

--- Branded/__main__.py
+++ Branded/__main__.py
@@ -3,7 +3,7 @@ import importlib
 
 from pytgcalls import idle
 
-from . import King as client
+from . import Branded as client
 from .import PLUGINS, log
 from .plugins import ALL_PLUGINS
 
@@ -15,7 +15,7 @@ async def aditya():
     log.info("Importing all plugins ...")
     for all_plugin in ALL_PLUGINS:
         imported_plugin = importlib.import_module(
-            "King.plugins." + all_plugin)
+            "Branded.plugins." + all_plugin)
         if (hasattr(imported_plugin, "__NAME__"
            ) and imported_plugin.__NAME__):
             imported_plugin.__NAME__ = imported_plugin.__NAME__
