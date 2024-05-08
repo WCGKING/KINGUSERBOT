38779741f6c7a99081335e7e44fac863333fee62
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-05-08 07:14:30 +0530

    𝗕𝗥𝗔𝗡𝗗𝗘𝗗 𝗞𝗜𝗡𝗚 𝗨𝗦𝗘𝗥𝗕𝗢𝗧

--- Branded/modules/utilities/tgcalls.py
+++ Branded/modules/utilities/tgcalls.py
@@ -1,4 +1,5 @@
-from pytgcalls.types import Update
+from pytgcalls import filters
+from pytgcalls.types import ChatUpdate, Update
 
 from . import queues
 from ..clients.clients import app, call
@@ -6,14 +7,21 @@ from .streams import run_stream, close_stream
 
 
 async def run_async_calls():
-    @call.on_left()
-    @call.on_kicked()
-    @call.on_closed_voice_chat()
-    async def stream_services_handler(_, chat_id: int):
-        return await close_stream(chat_id)
+    @call.on_update(
+        filters.chat_update(
+            ChatUpdate.Status.CLOSED_VOICE_CHAT
+        )
+    )
+    @call.on_update(
+        filters.chat_update(
+            ChatUpdate.Status.KICKED | ChatUpdate.Status.LEFT_GROUP,
+        ),
+    )
+    async def stream_services_handler(_, update: Update):
+        return await close_stream(update.chat_id)
     
     
-    @call.on_stream_end()
+    @call.on_update(filters.stream_end)
     async def stream_end_handler(_, update: Update):
         chat_id = update.chat_id
         queues.task_done(chat_id)
@@ -23,6 +31,6 @@ async def run_async_calls():
         file = check["file"]
         type = check["type"]
         stream = await run_stream(file, type)
-        return await call.change_stream(chat_id, stream)
+        return await call.play(chat_id, stream)
     
         
